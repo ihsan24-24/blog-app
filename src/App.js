f9d975ac73d893cd8d2427cc54594708d4d4373f
ihsan <hasansaygin24@gmail.com>
2022-11-17 10:59:39 +0300

    app router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import AppRouter from "./appRouter/AppRouter";
 
 import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <AppRouter />
     </div>
   );
 }
--- src/appRouter/AppRouter.jsx
+++ src/appRouter/AppRouter.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import NavbarComp from "../components/Navbar";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <div>
+      <NavbarComp />
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default AppRouter;
