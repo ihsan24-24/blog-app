d082e1ac7e1d0075fdded1c2a8470e438839d57b
ihsan <hasansaygin24@gmail.com>
2022-11-17 10:30:04 +0300

    starter fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter} from "react-router-dom";
-import NavbarComp from './components/Navbar';
+import React from "react";
+
+import { BrowserRouter } from "react-router-dom";
+import NavbarComp from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavbarComp/>
+        <NavbarComp />
         <h1>Legendcx</h1>
-     </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
