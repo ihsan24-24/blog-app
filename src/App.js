9b254d9dab0343c7cbf63b95b780540ebc5d98d3
ihsan <hasansaygin24@gmail.com>
2022-11-22 16:26:22 +0300

    redux-persist added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import AppRouter from "./appRouter/AppRouter";
 import { ToastContainer } from "react-toastify";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store/store";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <AppRouter />
-      <ToastContainer />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <AppRouter />
+            <ToastContainer />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </div>
   );
 }
